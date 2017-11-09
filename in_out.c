4e7e67c0231d294834a08ffcc95480ef4e3aeaa3
tremlab <rbalmert@gmail.com>
2017-11-09 14:28:34 -0800

    standard in/out, getchar putchar

---
+++ in_out.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+	int c;
+
+	// below asks the terminal for text input -- zero to many characters e.g monkey
+	c = getchar();  
+
+	while (c != EOF) {
+		putchar(c);
+		printf("\n");
+		//  below advances one letter OF THE GIVEN STRING??? e.g. m, then o, then n...
+		//  how can the same methos do both these things?
+		c = getchar();
+	}
+}
+
+
+// int main()
+// {
+// 	int word, c;
+
+
+// 	word = getchar(); 
+// 	// nope - the below fails :( 
+// 	// error: too many arguments to function call, expected 0, have 1
+//                 // c = getchar(word);
+// 	c = getchar(word); 
+
+// 	while (c != EOF) {
+// 		putchar(c);
+// 		printf("\n");
+// 		c = getchar(word);
+// 	}
+// }
+
