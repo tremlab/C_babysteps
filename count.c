8451574b452a2c2296342e0b1297f7b1800f1ad0
tremlab <rbalmert@gmail.com>
2017-11-16 14:55:52 -0800

    adding brackets to while loop

--- count.c
+++ count.c
@@ -1,37 +1,21 @@
 #include <stdio.h>
 
+
+
 int main()
 {
 	
 	long nc;
-	int l;
-
 	nc = 0;
-	l = getchar();
-	printf("word = %i\n", l);
 
-	while ((l = getchar()) != EOF)
-		putcahr(l)
-		printf("count = %ld\n", nc);
+	while (getchar() != EOF) {
+		printf("%.0ld\n", nc); 
 		++nc;
+	}
 
 	printf("final:  %ld\n", nc);
 	return 0;
 }
-
-// int main()
-// {
-	
-// 	long nc;
-// 	nc = 0;
-
-// 	while (getchar()) != EOF)
-// 		printf("count = %ld\n", nc);
-// 		++nc;
-
-// 	printf("final:  %ld\n", nc);
-// 	return 0;
-// }
 
 
 // int main()
@@ -43,4 +27,28 @@ int main()
 // 	printf("final count: %.0f\n", nc);
 // }
 
+
+
+/* below, trying to write code that would print each letter and the current count so I could watch each increment.  not working yet.  :P
+*/
+
+
+// int main()
+// {
+	
+// 	long nc;
+// 	int l;
+
+// 	nc = 0;
+// 	l = getchar();
+// 	printf("word = %i\n", l);
+
+// 	while ((l = getchar()) != EOF)
+// 		putchar(l);
+// 		printf("count = %ld\n", nc);
+// 		++nc;
+
+// 	printf("final:  %ld\n", nc);
+// 	return 0;
+// }
 
